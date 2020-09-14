c657aff42bbaa347e74d5f663a71741852b29215
andrew-bbq <awbennett@wisc.edu>
2020-09-14 00:04:37 -0500

    Define schema/model stuff

--- server.js
+++ server.js
@@ -24,6 +24,26 @@ const db = mongoose.connection;
 db.on('error', error => console.error(error));
 db.once('open', () => console.log('Connected to Mongoose'));
 
+// define schema
+var Schema = mongoose.Schema;
+
+var StreamerSchema = new Schema({
+    TwitchId: String,
+    SpotifyId: String
+});
+
+// compile model from schema
+var StreamerModel = mongoose.model("Streamer", StreamerSchema);
+
+// test create instance of model
+var StreamInstance = new StreamerModel({TwitchId: "abcd", SpotifyId: "1234"});
+StreamInstance.save(function(err) {
+    if (err) return handleError(err);
+    //saved
+});
+
+console.log(StreamInstance.TwitchId);
+
 app.use('/', indexRouter);
 
 app.listen(process.env.PORT || 3000);
